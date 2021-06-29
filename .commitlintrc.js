5900cc142174cec539ba2632eb2e69e0d54b8361
chenchuyin <1611804874@qq.com>
2021-06-29 21:52:26 +0800

    delete: 删除husky下的gitignore

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -4,7 +4,7 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        'type-enum': [2, 'always', ['upd', 'feat', 'fix', 'refactor', 'docs', 'chore', 'style', 'revert']],
+        'type-enum': [2, 'always', ['upd', 'feat', 'fix', 'refactor', 'docs', 'chore', 'style', 'revert', 'delete']],
         'type-case': [0],
         'type-empty': [0],
         'scope-empty': [0],
